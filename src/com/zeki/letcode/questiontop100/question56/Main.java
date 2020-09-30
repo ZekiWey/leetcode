21949dd77ee38cbbdc3ad7d8933f0767866bd94d
DESKTOP-5FL56T6\Zeki <1251921323@qq.com>
2020-09-30 16:50:45 +0800

    +++

--- src/com/zeki/letcode/questiontop100/question56/Main.java
+++ src/com/zeki/letcode/questiontop100/question56/Main.java
@@ -1,5 +1,10 @@
 package com.zeki.letcode.questiontop100.question56;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * 给出一个区间的集合，请合并所有重叠的区间。
  *
@@ -19,20 +24,70 @@ package com.zeki.letcode.questiontop100.question56;
 public class Main {
 
     public int[][] merge(int[][] intervals) {
+        if(intervals.length == 0){
+            return intervals;
+        }
+        //根据区间首元素和尾元素从小到大排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] interval1, int[] interval2) {
+                int i = interval1[0] - interval2[0];
+                if(i > 0){
+                    return 1;
+                }
+                if(i == 0){
+                    return interval1[1] - interval2[1];
+                }
+                return -1;
+            }
+        });
+        /*for (int i = 0; i < intervals.length - 1; i++) {
+            for (int j = i + 1; j < intervals.length; j++) {
+                if(intervals[i][0] > intervals[j][0]){
+                    //交换
+                    int[] temp = intervals[i];
+                    intervals[i] = intervals[j];
+                    intervals[j] = temp;
+                }
+                if(intervals[i][0] == intervals[j][0]){
+                    if(intervals[i][1] > intervals[j][1]){
+                        //交换
+                        int[] temp = intervals[i];
+                        intervals[i] = intervals[j];
+                        intervals[j] = temp;
+                    }
+                }
+            }
+        }*/
+        List<int[]> res = new ArrayList<>();
+        int begin = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if(intervals[i][0] <= end){
+                end = intervals[i][1] > end ? intervals[i][1] : end;
+                continue;
+            }
+            res.add(new int[]{begin,end});
+            begin = intervals[i][0];
+            end = intervals[i][1];
+        }
+        res.add(new int[]{begin,end});
 
-        return null;
+        return res.toArray(new int[res.size()][]);
     }
 
     public static void main(String[] args) {
-        int[][] intervals = new int[][] {{1,3},{2,6},{8,10},{15,18}};
+        int[][] intervals = new int[][] {{8,10},{1,3},{15,18},{2,6}};
+        int[][] intervals1 = new int[][] {{1,4}};
         Main main = new Main();
-        main.merge(intervals);
+        int[][] merge = main.merge(intervals);
+        for (int i = 0; i < merge.length; i++) {
+            System.out.println(Arrays.toString(merge[i]));
+        }
     }
 
     /**
-     * z  z y  y  z   
-     * 1，2，3，6，8，10
-     * 1，2，3，5，8，10，15，18
+     * 1  6 8 10 15 18
      *
      * 1，3
      * 2，5
