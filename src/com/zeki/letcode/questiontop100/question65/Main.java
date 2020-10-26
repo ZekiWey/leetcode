bac92fb36de028456897b463b3d8c785afbdd1c9
DESKTOP-5FL56T6\Zeki <1251921323@qq.com>
2020-10-26 18:15:15 +0800

    +++

--- src/com/zeki/letcode/questiontop100/question65/Main.java
+++ src/com/zeki/letcode/questiontop100/question65/Main.java
@@ -1,17 +1,111 @@
 package com.zeki.letcode.questiontop100.question65;
 
 /**
+ * 65.验证给定的字符串是否可以解释为十进制数字。
  * @author Zeki
  * @date 2020/10/19 17:48
  */
 public class Main {
     public boolean isNumber(String s) {
+        s = s.trim();
+        s = s.toLowerCase();
 
-        return false;
+        if(s.length() == 0){
+            return false;
+        }
+
+        boolean f = true;
+        boolean point = false;
+        boolean e = false;
+        int ep = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            if(c < '0' || c > '9'){
+                if(c == '-' || c == '+'){
+                    if(e) {
+                        if (i != ep + 1 || i >= s.length() - 1) {
+                            f = false;
+                            break;
+                        }
+                    }else {
+                        if(i != 0 || i >= s.length() - 1){
+                            f = false;
+                            break;
+                        }
+                        //后一位必须是数字或小数点
+                        int t = s.charAt(i + 1);
+                        if(t < '0' || t > '9'){
+                            if(t != '.'){
+                                f = false;
+                                break;
+                            }
+                        }
+
+                    }
+                } else if(c == '.'){
+                    if(point || e){
+                        f = false;
+                        break;
+                    }else {
+                        if(i == 0 && i >= s.length() - 1){
+                            f = false;
+                            break;
+                        }
+                        //前一位和后一位必须是数字
+                        int t;
+                        boolean pism = false;
+                        if (i > 0){
+                            t = s.charAt(i - 1);
+                            if(t < '0' || t > '9'){
+                                if( t != '-' && t != '+'){
+                                    f = false;
+                                    break;
+                                }
+                            }else {
+                                pism = true;
+                            }
+                        }
+                        if(i < s.length() - 1) {
+                            t = s.charAt(i + 1);
+                            if (t < '0' || t > '9') {
+                                if (t != 'e' || !pism) {
+                                    f = false;
+                                    break;
+                                }
+                            }
+                        }else {
+                            if(!pism){
+                                f = false;
+                                break;
+                            }
+                        }
+                    }
+                    point = true;
+                } else if(c == 'e'){
+                    if(e){
+                        f = false;
+                        break;
+                    }else {
+                        if(i == 0 || i >= s.length() - 1){
+                            f = false;
+                            break;
+                        }
+                    }
+                    e = true;
+                    ep = i;
+                } else {
+                    //其他字符
+                    f = false;
+                    break;
+                }
+            }
+        }
+        return f;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.isNumber("-5e2");
+        System.out.println(main.isNumber("  +e1  "));
     }
 }
---
+++ src/com/zeki/letcode/questiontop100/question68/Main.java
@@ -0,0 +1,133 @@
+package com.zeki.letcode.questiontop100.question68;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个单词数组和一个长度 maxWidth，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
+ * 你应该使用“贪心算法”来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
+ * 要求尽可能均匀分配单词间的空格数量。如果某一行单词间的空格不能均匀分配，则左侧放置的空格数要多于右侧的空格数。
+ * 文本的最后一行应为左对齐，且单词之间不插入额外的空格。
+ *
+ * @author Zeki
+ * @date 2020/10/26 14:05
+ */
+public class Main {
+
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int p = 0;
+        while (p < words.length){
+            //选择一行的单词
+            int currLen = 0;
+            int currP = p;
+
+            while (currLen - 1 < maxWidth && currP < words.length){
+                if(currLen + words[currP].length() > maxWidth){
+                    break;
+                }
+                currLen = currLen + words[currP].length() + 1;
+                currP++;
+            }
+            //计算空格数量
+            int wordsNum = currP - p;
+            int spaceNum = (maxWidth - currLen - 1) + wordsNum - 1;
+            int ts = spaceNum;
+            System.out.println(wordsNum + "-" + spaceNum);
+            //生成一行
+            StringBuilder sb = new StringBuilder();
+            for(;p < currP;p++){
+                sb.append(words[p]);
+                //添加空格
+                int tempSpaceNum;
+                if(spaceNum == 0){
+                    tempSpaceNum = 0;
+                }else {
+                    if(spaceNum % (wordsNum - 1) == 0){
+                        tempSpaceNum = spaceNum / (wordsNum - 1);
+                    }else {
+                        spaceNum = spaceNum - spaceNum % (wordsNum - 1);
+                        tempSpaceNum = spaceNum / (wordsNum - 1) + spaceNum % (wordsNum - 1);
+                    }
+                }
+                for (int i = 0; i < tempSpaceNum; i++) {
+                    sb.append(' ');
+                }
+
+            }
+            res.add(sb.toString());
+        }
+        return res;
+    }
+
+
+
+    public List<String> fullJustify1(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int p = 0;
+        while (p < words.length){
+            //选择一行的单词
+            int currP = p;
+            int currLen = 0;
+            int wordNum = 0;
+
+            while (currLen < maxWidth && currP < words.length){
+                if(currLen + wordNum + words[currP].length() > maxWidth){
+                    break;
+                }
+                currLen = currLen + words[currP].length();
+                wordNum++;
+                currP++;
+            }
+            //需要添加空格的数量
+            int spaceNumSum = maxWidth - currLen;
+            //间隙数量
+            int intervalNum = wordNum - 1 == 0 ? 1 : wordNum - 1;
+
+            StringBuilder sb = new StringBuilder();
+            for(;p < currP;p++){
+                //添加单词
+                sb.append(words[p]);
+                //添加空格
+                if(currP >= words.length){
+                    sb.append(' ');
+                }else {
+                    if(intervalNum > 0){
+                        int tempSpaceNum;
+                        if(spaceNumSum % intervalNum == 0){
+                            tempSpaceNum = spaceNumSum / intervalNum;
+                        }else {
+                            tempSpaceNum = spaceNumSum / intervalNum + spaceNumSum % intervalNum;
+                        }
+                        for (int i = 0; i < tempSpaceNum; i++) {
+                            sb.append(' ');
+                        }
+                        spaceNumSum = spaceNumSum - tempSpaceNum;
+                        intervalNum--;
+                    }
+                }
+            }
+            if(currP >= words.length){
+                //最后一行补齐空格
+                int num = maxWidth - sb.length();
+                for (int i = 0; i < num; i++) {
+                    sb.append(' ');
+                }
+            }
+            res.add(sb.toString());
+
+        }
+        return res;
+    }
+
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        String[] words = new String[]{"What","must","be","acknowledgment","shall","be"};
+        List<String> list = main.fullJustify1(words, 16);
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
